384833b1b034d8c05782b233582c0a35ecb2d1b2
Matt Borland <matt.borland1@gmail.com>
2014-02-06 23:39:32 -0500

    Added Median Calc

--- v1.3/datafetch/aprs_read.py
+++ v1.3/datafetch/aprs_read.py
@@ -10,7 +10,10 @@ class aprs_read(object):
         self.lon  = []  # used only to print prev points
         self.vnor = []  # wind speed north (m/s)
         self.veas = []  # wind speed east (m/s)
-        self.wspd = []  # wind speed (m/s)
+        self.wspd = []  # wind speed (m/s) APRSSpeed
+
+
+
 
     def read_xml(self, filename):
         file = open(filename, 'r')
@@ -53,6 +56,7 @@ class aprs_read(object):
                         else:
                             if not burst3:
                                 burst1 = burst2 = False
+                    
                     self.time.append(time)
                     self.lat.append(lat)
                     self.lon.append(lon)
@@ -61,11 +65,39 @@ class aprs_read(object):
                     lonold = lon
                     hghtold = alt
                     print('{0} {1} {2}\n'.format(time, lat, lon))
-
-                    #Port from driver.pl
         file.close()
 
 
+
+
+    #Median Filter
+    def median_filter(self):
+        iAPRS = 0
+        
+        for speed in self.wspd
+            #First assume that the middle value is the median
+            iMedian = iAPRS  
+
+            if iAPRS > 0 and iAPRS < len(self.wspd) - 1:
+                if self.wspd[iAPRS - 1] > self.wspd[iAPRS] and self.wspd[iAPRS - 1] < self.wspd[iARPS + 1] or \
+                   self.wspd[iAPRS - 1] < self.wspd[iAPRS] and self.wspd[iAPRS - 1] > self.wspd[iAPRS + 1]:
+                    iMedian = iAPRS - 1
+
+                if self.wspd[iAPRS + 1] > self.wspd[iAPRS] and self.wspd[iAPRS + 1] < self.wspd[iAPRS - 1] or \ 
+                   self.wspd[iAPRS + 1] < self.wspd[iAPRS] and self.wspd[iAPRS + 1] > self.wspd[iAPRS - 1]:
+                    iMedian = iAPRS + 1
+            
+            self.vnor[iAPRS] = self.vnor[iMedian]
+            self.veas[iARPS] = self.veas[iMedian]
+
+            iAPRS = iAPRS + 1
+
+    
+    
+    
+    #Replace the ARL data with APRS data
+    def overwrite_data(self):
+        
 if __name__ == "__main__":
     aprs = aprs_read()
     aprs.read_xml('aprs.xml')
